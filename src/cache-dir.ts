a75819c8f131e0d67e035bc5ee0b78b52250abf4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-09 12:47:44 +0800

    init commit: extract from @beenotung/tslib

---
+++ src/cache-dir.ts
@@ -0,0 +1,83 @@
+import { mkdirSync, readFileSync, statSync, writeFileSync } from 'fs'
+import { readFile, writeFile, stat } from 'fs/promises'
+import { appendIgnoreLine } from './ignore'
+import { join } from 'path'
+
+export type CacheDirOptions = {
+  /** @default '.cache' */
+  dir?: string
+
+  /** @default 15*60*1000 (15 minutes) */
+  expireInterval?: number
+
+  /** @default '.gitignore' (false to skip auto append) */
+  gitignore?: string | false
+}
+
+/**
+ * @description Cache and re-use computation result with custom filename and directory.
+ * */
+export class CacheDir {
+  dir: string
+  expireInterval: number
+  constructor(options?: CacheDirOptions) {
+    this.dir = options?.dir || '.cache'
+    this.expireInterval = options?.expireInterval || 15 * 60 * 1000
+    mkdirSync(this.dir, { recursive: true })
+    if (options?.gitignore !== false) {
+      appendIgnoreLine(options?.gitignore || '.gitignore', this.dir)
+    }
+  }
+
+  runSync(args: { filename: string; fn: () => string; as?: 'string' }): string
+  runSync(args: { filename: string; fn: () => Buffer; as?: 'buffer' }): Buffer
+  runSync(args: {
+    filename: string
+    fn: () => string | Buffer
+    as?: 'string' | 'buffer'
+  }): string | Buffer {
+    const file = join(this.dir, args.filename)
+    try {
+      const stats = statSync(file)
+      const passedTime = Date.now() - stats.mtimeMs
+      if (passedTime < this.expireInterval) {
+        return readFileSync(file)
+      }
+    } catch (error) {
+      // file not exists or name clash with directory
+    }
+    const result = args.fn()
+    writeFileSync(file, result)
+    return result
+  }
+
+  runAsync(args: {
+    filename: string
+    fn: () => Promise<string>
+    as?: 'string'
+  }): Promise<string>
+  runAsync(args: {
+    filename: string
+    fn: () => Promise<Buffer>
+    as?: 'buffer'
+  }): Promise<Buffer>
+  async runAsync(args: {
+    filename: string
+    fn: () => Promise<string | Buffer>
+    as?: 'string' | 'buffer'
+  }): Promise<string | Buffer> {
+    const file = join(this.dir, args.filename)
+    try {
+      const stats = await stat(file)
+      const passedTime = Date.now() - stats.mtimeMs
+      if (passedTime < this.expireInterval) {
+        return await readFile(file)
+      }
+    } catch (error) {
+      // file not exists or name clash with directory
+    }
+    const result = await args.fn()
+    writeFile(file, result)
+    return result
+  }
+}
